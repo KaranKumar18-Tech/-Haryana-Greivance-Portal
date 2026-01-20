47248b55e0ee3644ba5803dbea2ab9e332f2e5e9
Karan Kumar <karan@Karans-MacBook-Air.local>
2026-01-20 15:51:37 +0530

    Fix CM image - use Postimg direct link

--- components/Views.tsx
+++ components/Views.tsx
@@ -77,7 +77,7 @@ export const HomeView: React.FC<{ onFileGrievance: () => void; onTrack: () => vo
             {/* Image Content - Aligned Right */}
             <div className="w-full md:w-1/2 flex flex-col items-center md:items-end mt-8 md:mt-0">
               <img
-                src="https://i.ibb.co/xKBrGpMw/nayab-singh-saini.jpg"
+                src="https://i.postimg.cc/T16nG9qT/image.png"
                 alt="Hon. Nayab Singh Saini, Chief Minister of Haryana"
                 className="w-48 h-48 md:w-64 md:h-64 object-cover rounded-xl shadow-lg"
                 loading="lazy"
